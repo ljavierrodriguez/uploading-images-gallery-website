9259850332e9ccfa51cd91edaea23cad3f5c74d4
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-10-19 18:55:16 +0000

    Uploading multiple images

--- back/src/routes/galleries.py
+++ back/src/routes/galleries.py
@@ -41,6 +41,32 @@ def galleries():
         return jsonify(gallery_image.serialize()), 200
 
 
+    """ if request.method == 'POST':
+        
+        title = request.form['title']
+        active = request.form['active']
+        images = request.files['images']
+
+        print(images)
+        data = []
+
+        for image in images:
+
+          resp = cloudinary.uploader.upload(image, folder="gallery")
+
+          if not resp: return jsonify({ "msg": "error uploading image"}), 400
+
+          gallery_image = Gallery()
+          gallery_image.title = title
+          gallery_image.active = True if active == 'true' else False
+          gallery_image.filename = resp['secure_url']
+          gallery_image.save()
+
+          data.append(gallery_image.serialize())
+
+        return jsonify(data.serialize()), 200 """
+
+
 @bpGI.route('/galleries/<int:id>', methods=['PUT'])
 def galleries_update_active(id):
 
